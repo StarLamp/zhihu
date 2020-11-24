3408818b3ee06cbccb1e4bcbb2d4bff0405ab5c9
StarLamp <875652194@qq.com>
2020-11-24 17:32:21 +0800

    1.0

--- zhihu/spiders/zhihu_com.py
+++ zhihu/spiders/zhihu_com.py
@@ -1,41 +1,52 @@
+import hashlib
+import time
+
 import scrapy
+from scrapy import Request, Selector, FormRequest
+import hmac
 
 
 class ZhihuComSpider(scrapy.Spider):
     name = 'zhihu.com'
     allowed_domains = ['zhihu.com']
     start_urls = ['http://zhihu.com/']
+    login_url = 'https://www.zhihu.com/api/v3/oauth/sign_in'
+
+    def __init__(self, name=None, **kwargs):
+        super().__init__(name, kwargs)
+        self.login_data = {
+            'grant_type': "password",
+            'client': 'c3cef7c66a1843f8b3a9e6a1e3160e20',
+            'timestamp': str(int(time.time()*1000)),
+            'source':'com.zhihu.web',
+        }
 
     def parse(self, response):
         pass
-class UserInfoItem(scrapy.Item):
-# define the fields for your item here like:
-# id
-    user_id = scrapy.Field()
-# 头像img
-    user_image_url = scrapy.Field()
-# 姓名
-    name = scrapy.Field()
-# 居住地
-    location = scrapy.Field()
-# 技术领域
-    business = scrapy.Field()
-# 性别
-    gender = scrapy.Field()
-# 公司
-    employment = scrapy.Field()
-# 职位
-    position = scrapy.Field()
-# 教育经历
-    education = scrapy.Field()
-# 我关注的人数
-    followees_num = scrapy.Field()
-# 关注我的人数
-    followers_num = scrapy.Field()
-class RelationItem(scrapy.Item):
-# 用户id
-    user_id =scrapy.Field()
-# relation 类型
-    relation_type =scrapy.Field()
-# 和我有关系的人的id列表
-    relations_id = scrapy.Field()
+
+    def start_requests(self):
+        #首先进入登录界面
+        login_url = 'https://www.zhihu.com'
+        header={
+            'user-agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36',
+            'content-type':'application/x-www-form-urlencoded'
+        }
+        #界面登录
+        return [Request(login_url,callback=self.start_login,meta={'cookiejar':1})]
+
+    def start_login(self,response):
+       return [FormRequest(
+           self.login_url,
+           method='POST',
+
+       )]
+
+
+    def _get_signature(self):
+        ha = hmac.new(b'd1b964811afb40118a12068ff74a12f4',digestmod=hashlib.sha1())
+        grant_type = self.login_data['grant_type']
+        client_id = self.login_data['client_id']
+        source = self.login_data['source']
+        timestamp = self.login_data['timestamp']
+        ha.update((grant_type+client_id+source+timestamp).encode('utf-8'))
+        return ha.hexdigest()
